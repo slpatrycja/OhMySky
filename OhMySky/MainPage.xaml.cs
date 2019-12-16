ff26590952137972c9a2048a1f4686d1b061ef30
Patrycja Słabosz <sl.patrycja.of@gmail.com>
2019-12-16 16:33:01 +0100

    Add background picture, fetch data only for current day

--- OhMySky/MainPage.xaml.cs
+++ OhMySky/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
 using System.IO;
 using System.Reflection;
 
@@ -15,10 +17,14 @@ namespace OhMySky
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        SKPaint backgroundFillPaint = new SKPaint { Style = SKPaintStyle.Fill };
+
         public MainPage()
         {
             InitializeComponent();
+            CreateShader();
             GetAsteroidsData();
+
         }
 
         public async void GetAsteroidsData()
@@ -31,22 +37,28 @@ namespace OhMySky
         string GenerateRequestUri(string endpoint)
         {
             string requestUri = endpoint;
-            requestUri += $"?start_date={getStartDate()}&end_date={getEndDate()}";
+            requestUri += $"?start_date={GetCurrentDate()}&end_date={GetCurrentDate()}";
             requestUri += $"&api_key={Constants.OpenWeatherMapAPIKey}";
             return requestUri;
         }
 
-        private string getStartDate()
+        private string GetCurrentDate()
         {
             DateTime dateTime = DateTime.UtcNow.Date;
             return dateTime.ToString("yyyy-MM-dd");
         }
 
-        private string getEndDate()
+        private void CreateShader()
         {
-            DateTime dateTime = DateTime.UtcNow.Date;
-            DateTime dt = dateTime.AddDays(7);
-            return dt.ToString("yyyy-MM-dd");
+            System.Reflection.Assembly assembly = GetType().GetTypeInfo().Assembly;
+            using (Stream stream = assembly.GetManifestResourceStream("OhMySky.Background.jpg"))
+            using (SKManagedStream skStream = new SKManagedStream(stream))
+            using (SKBitmap bitmap = SKBitmap.Decode(skStream))
+            using (SKShader shader = SKShader.CreateBitmap(bitmap, SKShaderTileMode.Mirror, SKShaderTileMode.Mirror))
+            {
+                backgroundFillPaint.Shader = shader;
+            }
         }
+
     }
 }
