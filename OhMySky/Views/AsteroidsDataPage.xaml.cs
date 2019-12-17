07061991464223534b09ab374f9c737696f316d1
ajeffika <jan.mistarz6767@gmail.com>
2019-12-17 22:04:40 +0200

    add unsaved files

--- OhMySky/Views/AsteroidsDataPage.xaml.cs
+++ OhMySky/Views/AsteroidsDataPage.xaml.cs
@@ -17,27 +17,6 @@ namespace OhMySky.Views
 			//InitializeComponent ();
             //BindingContext = new AsteroidsDataViewModel();
 
-            async void AsteroidsDataPage()
-            {
-                RestService restService = new RestService();
-                AsteroidData asteroidData = await restService.GetAsteroidData(GenerateRequestUri(Constants.NasaApiEndpoint));
-
-                BindingContext = asteroidData;
-            }
-
-            string GenerateRequestUri(string endpoint)
-            {
-                string requestUri = endpoint;
-                requestUri += $"?start_date={GetCurrentDate()}&end_date={GetCurrentDate()}";
-                requestUri += $"&api_key={Constants.APIKey}";
-                return requestUri;
-            }
-
-             string GetCurrentDate()
-            {
-                return DateTime.UtcNow.Date.ToString("yyyy-MM-dd");
-            }
-
-        }
+           
     }
 }
--- OhMySky/Views/MainPage.xaml.cs
+++ OhMySky/Views/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using OhMySky.Views;
+using OhMySky.ViewModels;
 using SkiaSharp;
 using SkiaSharp.Views.Forms;
 using System.IO;
