ce56bf2915882317b3054b1850196f3d474e68ef
ajeffika <jan.mistarz6767@gmail.com>
2019-12-17 23:26:03 +0200

    remade MVVM structutr

---
+++ OhMySky/AsteroidDataPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace OhMySky
+{
+	public partial class AsteroidDataPage : ContentPage
+	{
+		public AsteroidDataPage()
+		{
+			InitializeComponent();
+			BindingContext = new AsteroidDataViewModel();
+		}
+	}
+}
