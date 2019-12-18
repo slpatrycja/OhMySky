b1489176f082992c484c5c4bfc17f6413ba55a9d
Patrycja Słabosz <sl.patrycja.of@gmail.com>
2019-12-18 22:03:29 +0100

    wip

---
+++ OhMySky/Infrastructure/ViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reactive.Disposables;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace OhMySky.Infrastructure
+{
+    public abstract class ViewModel : ReactiveObject
+    {
+        CompositeDisposable deactivateWith;
+        protected CompositeDisposable DeactivateWith
+        {
+            get
+            {
+                if (this.deactivateWith == null)
+                    this.deactivateWith = new CompositeDisposable();
+
+                return this.deactivateWith;
+            }
+        }
+
+        protected CompositeDisposable DestroyWith { get; } = new CompositeDisposable();
+
+
+        public virtual void OnDisappearing()
+        {
+            this.deactivateWith?.Dispose();
+            this.deactivateWith = null;
+        }
+
+
+        public virtual void Destroy()
+        {
+            this.DestroyWith?.Dispose();
+        }
+
+
+        [Reactive] public bool IsBusy { get; protected set; }
+    }
+}
