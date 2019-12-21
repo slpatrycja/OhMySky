24e7970ce9ce9974e49b4ef319c7c9dbf43e93b9
Dariusz Mrok <mrokdariusz@gmail.com>
2019-12-21 13:00:04 +0100

    Fix displaying observable collection in view model

---
+++ OhMySky/iAsteroid.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace OhMySky
+{
+    public class iAsteroid
+    {
+        public string Name { get; set; }
+        public string id { get; set; }
+        public bool IsPotentiallyHazardous { get; set; }
+    }
+}
