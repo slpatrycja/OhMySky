fd8a09fbba148ddae299f80230225718d6098bbb
Jan Mistarz <jan.mistarz6767@gmail.com>
2019-12-30 23:40:53 +0200

    added tests

---
+++ NUnitOhMySky/ViewModels/AsteroidDataViewModelTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using OhMySky;
+
+namespace NUnitOhMySky.Tests
+{
+    [TestFixture]
+    class AsteroidDataViewModelTests
+    {
+        [Test]
+        public void GetDataTest()
+        {
+            var asteroidDataViewModel = new AsteroidDataViewModel();
+            Assert.AreEqual(asteroidDataViewModel.GetAsteroidData, );
+            Assert.IsNotNull(asteroidDataViewModel.GetAsteroidData());
+        }
+        [Test]
+        public void GenerateUriTest()
+        {
+
+        }
+    }
+}
